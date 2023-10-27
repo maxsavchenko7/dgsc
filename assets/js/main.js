db3bc8193e2c64f7da243cac9092245fbd62f429
Max <maxsavch07@gmail.com>
2023-10-27 18:53:37 +0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+jQuery(($) => {
+
+    $('.search-form__submit').on('click', function (e) {
+        if ($(this).parent('.search-form').hasClass('active')) return;
+        
+        e.preventDefault();
+        $(this).parent().addClass('active');
+    });
+
+    $(document).mouseup( function(e){
+		let searchForm = $('.search-form');
+
+		if ( !searchForm.is(e.target) && !searchForm.has(e.target).length ) { 
+			searchForm.removeClass('active');
+		}
+	});
+
+});
